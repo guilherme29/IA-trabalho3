2cb4da45b36c6b1ccc8b05ed9bcc5d69ff82e4bb
Guilherme Martins Amado <gui.amado29@gmail.com>
2018-04-13 17:39:58 +0100

    mudança de nomenclatura de algumas funções e tipos de dados de modo a refletir a quantidade de palavras duma frase.

--- grammar.hs
+++ grammar.hs
@@ -53,29 +53,27 @@ instance Show Palavra where
   show (Conjm_p x)  = "conjuction(" ++ x ++ ")"
   show (Prop x)     = "propostion(" ++ x ++ ")"
 
-data Sentence = Sentence Palavra Palavra Palavra
-instance Show Sentence where
-    show (Sentence d n v) = "sentence(" ++ (show d) ++ "," ++ (show n) ++ "," ++ (show v) ++ ")"
+data Sentence3 = Sentence3 Palavra Palavra Palavra
+instance Show Sentence3 where
+    show (Sentence3 d n v) = "sentence(" ++ (show d) ++ "," ++ (show n) ++ "," ++ (show v) ++ ")"
 
-parse1 :: [String] -> Sentence
-parse1 [x1,x2,x3] = Sentence (parseDet x1) (parseNoun x2) (parseVerb x3)
---parse [x1,x2,x3,x4,x5] = Sentence (parseDet x1) (parseNoun x2) (parseVerb x3) (parseDet x4) (parseNoun x5)
+parse3 :: [String] -> Sentence3
+parse3 [x1,x2,x3] = Sentence3 (parseDet x1) (parseNoun x2) (parseVerb x3)
 
-evaluate1 :: [String] -> Bool
-evaluate1 xs = evaluate1aux (parse1 xs)
+evaluate3 :: [String] -> Bool
+evaluate3 xs = evaluate3aux (parse3 xs)
 
-evaluate1aux :: Sentence -> Bool
-evaluate1aux (Sentence x1 x2 x3) = isDetf x1 && isNounf x2 && isVerb x3
-                                ||isDetm x1 && isNounm x2 && isVerb x3
-                                ||isDetf_p x1 && isNounf_p x2 && isVerb_p x3
-                                ||isDetm_p x1 && isNounm_p x2 && isVerb_p x3
---evaluate2 (Sentence x1 x2 x3 x4 x5) = isDetf x1 && isNounf x2 && isVerb x3 && isDetf x4 && isNounf x5
---                                    ||isDetf x1 && isNounf x2 && isVerb x3 && isDetm x4 && isNounm x5
+evaluate3aux :: Sentence3 -> Bool
+evaluate3aux (Sentence3 x1 x2 x3)   = isDetf x1 && isNounf x2 && isVerb x3
+                                    ||isDetm x1 && isNounm x2 && isVerb x3
+                                    ||isDetf_p x1 && isNounf_p x2 && isVerb_p x3
+                                    ||isDetm_p x1 && isNounm_p x2 && isVerb_p x3
 
 
-data Sentence2 = Sentence2 Palavra Palavra Palavra Palavra Palavra
-instance Show Sentence2 where
-    show (Sentence2 d n v a1 a2) = "sentence(" ++ (show d) ++ "," ++ (show n) ++ "," ++ (show v) ++ "," ++ (show a1) ++ "," ++ (show a2) ++ ")"
+data Sentence5 = Sentence5 Palavra Palavra Palavra Palavra Palavra
+instance Show Sentence5 where
+    show (Sentence5 d n v a1 a2) = "sentence(" ++ (show d) ++ "," ++ (show n) ++ "," ++ (show v) ++ "," ++ (show a1) ++ "," ++ (show a2) ++ ")"
+
 
 --funções isX
 isDetf :: Palavra -> Bool
