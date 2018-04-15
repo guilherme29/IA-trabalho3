803a977619706dec6240bd0bdf6b233a230857db
Guilherme Martins Amado <gui.amado29@gmail.com>
2018-04-15 15:31:29 +0100

    adicionada função para dar print a um voo.

--- Flight.java
+++ Flight.java
@@ -15,16 +15,29 @@ class Flight{
     }
 
     void printFlight(int sourceNode, Arc flight){
-        System.out.println(Cities.getCity(sourceNode) + " -> " + Cities.getCity(flight.endNode))
-        int depH = (flight.departure*100)/60;  //departure hours
-        int depD = 0; //departure day
+        System.out.println(Cities.getCity(sourceNode) + " -> " + Cities.getCity(flight.endNode));
+        double depD = 0; //departure day
+        double depH = (flight.departure*100)/60;  //departure hours
         if(depH>=24){
             depD = depH /24;
             depH = depH %24;
         }
-        int depM = (flight.departure*100) %60; //departure mins
-        System.out.println("Departure: " + flight.departure);
-        //System.out.println("Arrival: " + flight)
+        double depM = (flight.departure*100) %60; //departure mins
+        System.out.print("Departure: " + getDay(depD) + " ");
+        System.out.printf("%d:%d%n",depH,depM);
+
+        double arrD = 0; //arrival day
+        double arrH = ((flight.departure + flight.flightTime-0.40)*100)/60; //arival hours
+        if(arrH>=24){
+            arrD = arrH /24;
+            arrH = arrH %24;
+        }
+        double arrM = ((flight.departure + flight.flightTime-0.40)*100)%60;
+        System.out.print("Arrival: " + getDay(arrD) + " ");
+        System.out.printf("%d:%d%n",arrH,arrM);
+        System.out.println("Flight Num:" + flight.flightNum);
+        System.out.println();
+
     }
 
     String getDay(int i){
@@ -38,5 +51,16 @@ class Flight{
             case 6: return "su";
             default: return null;
         }
+    }
+
+    String getDay(double i){
+        if(i == 0) return "mo";
+        else if(i == 1) return "tu";
+        else if(i == 2) return "we";
+        else if(i == 3) return "th";
+        else if(i == 4) return "fr";
+        else if(i == 5) return "sa";
+        else if(i == 5) return "su";
+        else return null;
     }
 }
