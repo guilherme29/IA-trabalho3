595bf1effa234ee388e87c91758b46afd6bc3b3f
Guilherme Martins Amado <gui.amado29@gmail.com>
2018-04-15 21:46:56 +0100

    novo printFlight.

--- Flight.java
+++ Flight.java
@@ -1,6 +1,5 @@
 import java.util.LinkedList;
 import java.util.PriorityQueue;
-import java.lang.Math.*;
 
 class Flight{
     public static void printDirectFlights(int sourceNode, int endNode, Graph map){
@@ -17,34 +16,9 @@ class Flight{
 
     public static void printFlight(int sourceNode, Arc flight){
         System.out.println(Cities.getCity(sourceNode) + " -> " + Cities.getCity(flight.endNode));
-
-        /*
-        double depD = 0; //departure day
-        double depH = (flight.departure*100)/60;  //departure hours
-        if(depH>=24){
-            depD = depH /24;
-            depH = depH %24;
-        }
-        double depM = (flight.departure*100) %60; //departure mins
-        //System.out.print("Departure: " + getDay(depD) + " ");
-        //System.out.printf("%f:%f%n",depH,depM);
-
-        double arrD = 0; //arrival day
-        double arrH = ((flight.departure + flight.flightTime-0.40)*100)/60; //arival hours
-        if(arrH>=24){
-            arrD = arrH /24;
-            arrH = arrH %24;
-        }
-        double arrM = ((flight.departure + flight.flightTime-0.40)*100)%60;
-        /*
-        System.out.print("Arrival: " + getDay(arrD) + " ");
-        System.out.printf("%f:%f%n",arrH,arrM);
-        System.out.println("Flight Num:" + flight.flightNum);
-        System.out.println();
-        */
-
-        //System.out.println(flight.departure + " -> " + flight.flightTime);
-        //System.out.println("Departure: " + depH + ":" + depM);
+        System.out.println("Departure: " + Time.getTime(flight.getDeparture()));
+        System.out.println("Arrival: " + Time.getTime(flight.getDeparture() + flight.getFlightTime() - 40));
+        System.out.println("Flight num: " + flight.getFlightNum());
     }
 
 }
