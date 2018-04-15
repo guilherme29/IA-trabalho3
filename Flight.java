14b15261dfda8c32449bb4af42e509f35019b0e8
Guilherme Martins Amado <gui.amado29@gmail.com>
2018-04-15 15:10:25 +0100

    adição de função getDay que retorna uma string com o dia dado um int.

--- Flight.java
+++ Flight.java
@@ -8,6 +8,35 @@ class Flight{
             System.out.println("Não há voos diretos.");
             return;
         }
-        System.out.println("Lista dos voos diretos entre");
+        System.out.println("Lista dos voos diretos entre: " + Cities.getCity(sourceNode) + " -> " + Cities.getCity(endNode));
+        for(Arc a : flightList){
+
+        }
+    }
+
+    void printFlight(int sourceNode, Arc flight){
+        System.out.println(Cities.getCity(sourceNode) + " -> " + Cities.getCity(flight.endNode))
+        int depH = (flight.departure*100)/60;  //departure hours
+        int depD = 0; //departure day
+        if(depH>=24){
+            depD = depH /24;
+            depH = depH %24;
+        }
+        int depM = (flight.departure*100) %60; //departure mins
+        System.out.println("Departure: " + flight.departure);
+        //System.out.println("Arrival: " + flight)
+    }
+
+    String getDay(int i){
+        switch(i){
+            case 0: return "mo";
+            case 1: return "tu";
+            case 2: return "we";
+            case 3: return "th";
+            case 4: return "fr";
+            case 5: return "sa";
+            case 6: return "su";
+            default: return null;
+        }
     }
 }
