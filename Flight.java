72e43fc0707786c2ee6fff0d0297a04fe93e26bf
Guilherme Martins Amado <gui.amado29@gmail.com>
2018-04-15 14:43:19 +0100

    encontrado o problema com LinkedLists e início da classe.

--- Flight.java
+++ Flight.java
@@ -1,8 +1,13 @@
-import java.util.Linkedlist;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 class Flight{
-    //void getDirectFlights(int sourceNode, int endNode, Graph map){
-    //    PriorityQueue<Arc> flightList = map.findAllArcs(sourceNode,endNode);
-
-    //}
+    void getDirectFlights(int sourceNode, int endNode, Graph map){
+        PriorityQueue<Arc> flightList = map.findAllArcs(sourceNode,endNode);
+        if(flightList.isEmpty()){
+            System.out.println("Não há voos diretos.");
+            return;
+        }
+        System.out.println("Lista dos voos diretos entre");
+    }
 }
